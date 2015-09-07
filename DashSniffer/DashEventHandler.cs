b80d281aea9f8648e658aaf2cbec5397940f8fd8
upnxt <chelfers@upnxt.com>
2015-09-06 22:20:18 -0500

    Initial commit

---
+++ DashSniffer/DashEventHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using PacketDotNet;
+using SharpPcap;
+
+namespace DashSniffer
+{
+    public class DashEventHandler
+    {
+        public static void Resolve(object sender, CaptureEventArgs e)
+        {
+            var result = Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+            var packet = result as EthernetPacket;
+
+            if (packet?.Type == EthernetPacketType.Arp)
+                PostDashEvent(packet);
+        }
+
+        private static void PostDashEvent(EthernetPacket packet)
+        {
+            if (((ARPPacket)packet.PayloadPacket).SenderProtocolAddress.ToString() != "0.0.0.0")
+                return;
+
+            var macAddress = packet.SourceHwAddress.ToString();
+            var button = ConfigurationManager.AppSettings.AllKeys.SingleOrDefault(m => m.Contains(macAddress));
+
+            if (button == null)
+                return;
+
+            var client = new HttpClient();
+            var values = new Dictionary<string, string>
+                {
+                    {"Event", ConfigurationManager.AppSettings[button] },
+                    {"MacAddress", macAddress },
+                    {"CreatedOn", DateTime.Now.ToString() }
+                };
+
+            var data = new FormUrlEncodedContent(values);
+            client.PostAsync("http://localhost:56719/your/api/url/here", data).ContinueWith(task =>
+            {
+                client.Dispose();
+            });
+        }
+    }
+}
---
+++ DashSniffer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using SharpPcap;
+using SharpPcap.LibPcap;
+
+namespace DashSniffer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var device = GetDevice();
+
+            Console.WriteLine($"Listening on {device.Name}");
+
+            device.OnPacketArrival += DashEventHandler.Resolve;
+            device.Open(DeviceMode.Promiscuous, 1000);
+            device.StartCapture();
+
+            Console.ReadLine();
+
+            device.StopCapture();
+            device.Close();
+
+        }
+
+        private static LibPcapLiveDevice GetDevice()
+        {
+            var devices = LibPcapLiveDeviceList.Instance;
+
+            if (devices == null || devices.Count < 1)
+                throw new Exception("No devices available to capture on.");
+
+            var preConfiguredDevice = devices.FirstOrDefault(m => string.Equals(m.Name, ConfigurationManager.AppSettings["DeviceName"], StringComparison.OrdinalIgnoreCase));
+
+            if (preConfiguredDevice != null)
+                return preConfiguredDevice;
+
+            var i = 0;
+
+            foreach (var dev in devices)
+            {
+                Console.WriteLine("{0}) {1} {2}", i, dev.Name, dev.Description);
+                i++;
+            }
+
+            Console.WriteLine();
+            Console.Write("listening device number: ");
+            i = int.Parse(Console.ReadLine());
+
+            var device = devices[i];
+            return device;
+        }
+    }
+}
